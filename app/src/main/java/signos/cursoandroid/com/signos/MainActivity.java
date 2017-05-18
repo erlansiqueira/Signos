2894caaaa999cb2f0c8fc9bb105668e565a50e8e
Cesar <erlan_siqueira@hotmail.com>
2017-05-18 19:00:47 -0300

    Tudo Pronto

--- app/src/main/java/signos/cursoandroid/com/signos/MainActivity.java
+++ app/src/main/java/signos/cursoandroid/com/signos/MainActivity.java
@@ -3,15 +3,25 @@ package signos.cursoandroid.com.signos;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private ListView listaSigno;
     private String[] signos = {
       "Aries", "Touro", "Gemeos", "Camcer", "Leao", "Virgem", "Libra",
             "Escorpiao", "Sagitario", "Capricornio", "Aquario", "Peixes"
+    };
+
+    private String[] perfis = {
+            "1", "2", "3", "4", "5", "6", "7", "8", "9",
+            "10", "11", "12"
+
+
     };
 
     @Override
@@ -28,6 +38,16 @@ public class MainActivity extends Activity {
         );
 
         listaSigno.setAdapter(adaptador);
+        listaSigno.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                int codigoPosicao = position;
+                Toast.makeText(getApplicationContext(), perfis[codigoPosicao], Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
 
 
     }
